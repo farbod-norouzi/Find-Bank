e03bd5de3f82aebf56a94688af149648f67b0abe
Senior Farbod <40601333+farbod-norouzi@users.noreply.github.com>
2022-05-16 15:48:26 +0430

    Add files via upload

---
+++ Bank.py
@@ -0,0 +1,69 @@
+import datetime
+
+print('\n',"Welcome To Card Bank Info",'\n')
+
+datetime_object = datetime.datetime.now()
+print(datetime_object,'\n')
+
+sixnum = input("Please enter the prefix of the desired bank: > ")
+
+if sixnum == "627353":
+    print('\n',"Bank : Tejarat")
+elif sixnum == "589210":
+    print('\n',"Bank : Sepah")
+elif sixnum == "603769":
+    print('\n',"Bank : Saderat")
+elif sixnum == "627648":
+    print('\n',"Bank : Tosee Saderat")
+elif sixnum == "610433":
+    print('\n',"Bank : Melat")
+elif sixnum == "628157":
+    print('\n',"Bank : Tosee Taavon") 
+elif sixnum == "627961":
+    print('\n',"Bank : Sanaat o Madan") 
+elif sixnum == "589463":
+    print('\n',"Bank : Refah") 
+elif sixnum == "621986":
+    print('\n',"Bank : Sman")
+elif sixnum == "628023":
+    print('\n',"Bank : Maskan") 
+elif sixnum == "603770":
+    print('\n',"Bank : Keshavarzi")     
+elif sixnum == "603799":
+    print('\n',"Bank : Meli")     
+elif sixnum == "627760":
+    print('\n',"Bank : Post Bank iran")     
+elif sixnum == "627412":
+    print('\n',"Bank : Karafarin")     
+elif sixnum == "622106":
+    print('\n',"Bank : Parsian") 
+elif sixnum == "627412":
+    print('\n',"Bank : Eghtesad Novin") 
+elif sixnum == "639347":
+    print('\n',"Bank : Pasargad") 
+elif sixnum == "639607":
+    print('\n',"Bank : Sarmaie")
+elif sixnum == "639346":
+    print('\n',"Bank : Sina")
+elif sixnum == "502806":
+    print('\n',"Bank : Shahr")
+elif sixnum == "621986":
+    print('\n',"Bank : Tat")
+elif sixnum == "627381":
+    print('\n',"Bank : Ansar")
+elif sixnum == "502938":
+    print('\n',"Bank : Dey")
+elif sixnum == "639370":
+    print('\n',"Bank : Mehr Eghtesad")
+elif sixnum == "507677":
+    print('\n',"Bank : Noor")
+elif sixnum == "628157":
+    print('\n',"Bank : Moaseseh Etebari Tosee")
+elif sixnum == "505801":
+    print('\n',"Bank : Kosar")
+elif sixnum == "606256":
+    print('\n',"Bank : Melal")
+elif sixnum == "606373":
+    print('\n',"Bank : Mehr Iranian")
+else:
+    print("Bank : Unknown Bank")
