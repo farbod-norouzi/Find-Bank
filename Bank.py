debde0afd60f4855eb7be9184b149560244fe069
Senior Farbod <40601333+farbod-norouzi@users.noreply.github.com>
2022-05-17 14:21:39 +0430

    Add files via upload

--- Bank.py
+++ Bank.py
@@ -1,69 +1,112 @@
 import datetime
+from colorama import Fore 
+import os
 
-print('\n',"Welcome To Card Bank Info",'\n')
+os.system("cls")
+print(Fore.RED + """ ______              _        _______ _           _             
+(____  \            | |      (_______|_)         | |            
+ ____)  ) ____ ____ | |  _    _____   _ ____   _ | | ____  ____ 
+|  __  ( / _  |  _ \| | / )  |  ___) | |  _ \ / || |/ _  )/ ___)
+| |__)  | ( | | | | | |< (   | |     | | | | ( (_| ( (/ /| |    
+|______/ \_||_|_| |_|_| \_)  |_|     |_|_| |_|\____|\____)_|    
+                                                                """,Fore.WHITE)
 
 datetime_object = datetime.datetime.now()
 print(datetime_object,'\n')
 
-sixnum = input("Please enter the prefix of the desired bank: > ")
+sixnum = input(Fore.LIGHTBLUE_EX + "Please enter the prefix of the desired bank: > ")
 
 if sixnum == "627353":
-    print('\n',"Bank : Tejarat")
+    print('\n',Fore.GREEN+ "Bank : Tejarat"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod",'\n')
 elif sixnum == "589210":
-    print('\n',"Bank : Sepah")
+    print('\n',Fore.GREEN+ "Bank : Sepah"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod",'\n')
 elif sixnum == "603769":
-    print('\n',"Bank : Saderat")
+    print('\n',Fore.GREEN+ "Bank : Saderat"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod",'\n')
 elif sixnum == "627648":
-    print('\n',"Bank : Tosee Saderat")
+    print('\n',Fore.GREEN+ "Bank : Tosee Saderat"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod",'\n')
 elif sixnum == "610433":
-    print('\n',"Bank : Melat")
+    print('\n',Fore.GREEN+ "Bank : Melat"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "628157":
-    print('\n',"Bank : Tosee Taavon") 
+    print('\n',Fore.GREEN+ "Bank : Tosee Taavon"+Fore.RESET) 
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "627961":
-    print('\n',"Bank : Sanaat o Madan") 
+    print('\n',Fore.GREEN+ "Bank : Sanaat o Madan"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n') 
 elif sixnum == "589463":
-    print('\n',"Bank : Refah") 
+    print('\n',Fore.GREEN+ "Bank : Refah"+Fore.RESET) 
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "621986":
-    print('\n',"Bank : Sman")
+    print('\n',Fore.GREEN+ "Bank : Sman"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "628023":
-    print('\n',"Bank : Maskan") 
+    print('\n',Fore.GREEN+ "Bank : Maskan"+Fore.RESET) 
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "603770":
-    print('\n',"Bank : Keshavarzi")     
+    print('\n',Fore.GREEN+ "Bank : Keshavarzi"+Fore.RESET)   
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')  
 elif sixnum == "603799":
-    print('\n',"Bank : Meli")     
+    print('\n',Fore.GREEN+ "Bank : Meli"+Fore.RESET)     
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "627760":
-    print('\n',"Bank : Post Bank iran")     
+    print('\n',Fore.GREEN+ "Bank : Post Bank iran"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')     
 elif sixnum == "627412":
-    print('\n',"Bank : Karafarin")     
+    print('\n',Fore.GREEN+ "Bank : Karafarin"+Fore.RESET)    
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n') 
 elif sixnum == "622106":
-    print('\n',"Bank : Parsian") 
+    print('\n',Fore.GREEN+ "Bank : Parsian"+Fore.RESET) 
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "627412":
-    print('\n',"Bank : Eghtesad Novin") 
+    print('\n',Fore.GREEN+ "Bank : Eghtesad Novin"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n') 
 elif sixnum == "639347":
-    print('\n',"Bank : Pasargad") 
+    print('\n',Fore.GREEN+ "Bank : Pasargad"+Fore.RESET) 
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "639607":
-    print('\n',"Bank : Sarmaie")
+    print('\n',Fore.GREEN+ "Bank : Sarmaie"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "639346":
-    print('\n',"Bank : Sina")
+    print('\n',Fore.GREEN+ "Bank : Sina"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "502806":
-    print('\n',"Bank : Shahr")
+    print('\n',Fore.GREEN+ "Bank : Shahr"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "621986":
-    print('\n',"Bank : Tat")
+    print('\n',Fore.GREEN+ "Bank : Tat"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "627381":
-    print('\n',"Bank : Ansar")
+    print('\n',Fore.GREEN+ "Bank : Ansar"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "502938":
-    print('\n',"Bank : Dey")
+    print('\n',Fore.GREEN+ "Bank : Dey"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "639370":
-    print('\n',"Bank : Mehr Eghtesad")
+    print('\n',Fore.GREEN+ "Bank : Mehr Eghtesad"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "507677":
-    print('\n',"Bank : Noor")
+    print('\n',Fore.GREEN+ "Bank : Noor"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "628157":
-    print('\n',"Bank : Moaseseh Etebari Tosee")
+    print('\n',Fore.GREEN+ "Bank : Moaseseh Etebari Tosee"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "505801":
-    print('\n',"Bank : Kosar")
+    print('\n',Fore.GREEN+ "Bank : Kosar"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "606256":
-    print('\n',"Bank : Melal")
+    print('\n',Fore.GREEN+ "Bank : Melal"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 elif sixnum == "606373":
-    print('\n',"Bank : Mehr Iranian")
+    print('\n',Fore.GREEN+ "Bank : Mehr Iranian"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
+elif sixnum == "639599":
+    print('\n',Fore.GREEN+ "Bank :Qavamin"+Fore.RESET)
+    print('\n'"Manufacturer:",Fore.MAGENTA+ "Senior.Farbod"+Fore.RESET,'\n')
 else:
-    print("Bank : Unknown Bank")
+    print(Fore.RED+ "Bank : Unknown Bank"+Fore.RESET)
+    print(Fore.YELLOW+ "Alert : Please Try Again"+Fore.RESET)
+    print("Manufacturer:",Fore.MAGENTA+ "Senior.Farbod",'\n')
